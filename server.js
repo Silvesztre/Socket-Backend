4127bffc2ce46c5feb5d437c686bece9d54fcb5d
Silve27 <k.prp0127@gmail.com>
2025-04-04 12:58:02 +0700

    add vercel.json for deployment

--- server.js
+++ server.js
@@ -11,19 +11,4 @@ app.use(express.json())
 const users = require('./routes/users')
 app.use('/users', users)
 
-const PORT = process.env.PORT || 5000
-
-const server = app.listen(
-    PORT,
-    console.log(
-        'Server running in ',
-        process.env.NODE_ENV,
-        'mode on port ',
-        PORT
-    )
-)
-
-process.on('unhandledRejection', (err, promise) => {
-    console.log(`Error:${err.message}`)
-    server.close(() => process.exit(1))
-})
+module.exports=app
